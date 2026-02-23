acf7d6a898930cb64e540760c0a9d449cd485ac1
github-action-benchmark <github@users.noreply.github.com>
2026-02-23 15:10:09 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for d023835bc052c076bbe1d92687def063ec603645

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771859409938,
+  "repoUrl": "https://github.com/likkidamu/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "damureddy3@gmail.com",
+            "name": "likkidamu",
+            "username": "likkidamu"
+          },
+          "committer": {
+            "email": "damureddy3@gmail.com",
+            "name": "likkidamu",
+            "username": "likkidamu"
+          },
+          "distinct": true,
+          "id": "d023835bc052c076bbe1d92687def063ec603645",
+          "message": "Initial commit",
+          "timestamp": "2026-02-23T10:06:59-05:00",
+          "tree_id": "63dc25781c845a66e2d3b44ec471b4cdb8d46588",
+          "url": "https://github.com/likkidamu/n8n-mcp/commit/d023835bc052c076bbe1d92687def063ec603645"
+        },
+        "date": 1771859408994,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
